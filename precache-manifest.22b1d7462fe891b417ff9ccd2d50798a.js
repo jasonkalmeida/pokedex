2d20068a6bc10acab69e2e45e2d1b6f5763e838e
jasonkalmeida <jasonkalmeida@gmail.com>
2019-08-12 16:07:47 -0700

    Updates

---
+++ precache-manifest.22b1d7462fe891b417ff9ccd2d50798a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430577c6c1f7f53ea877300c6557bad6",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "14e6380408a051f038fd",
+    "url": "/pokedex/static/css/main.1bb74a1b.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/pokedex/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "14e6380408a051f038fd",
+    "url": "/pokedex/static/js/main.7f65c856.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
